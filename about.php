42e93e945e089bc8363f3f15fdf91007e57f5f06
Alessandro Marina <alessandromarina@localhost>
2020-11-26 14:14:59 +0100

    Creation of a great part of all needed pages and index edit Signed-off-by: Alessandro Marina <alessandromarina@localhost>

---
+++ about.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<?php include_once 'header.php'; ?>
+
+<body>
+  <p>Aliquam blandit laoreet risus ac varius. Nam euismod mollis nunc a ultrices. Nulla suscipit, ipsum id posuere efficitur, metus neque mollis arcu, vel iaculis nibh dolor vitae mi. Nunc ac ante nec dolor congue ultricies at in leo. Etiam interdum, metus eget laoreet suscipit, augue eros pellentesque libero, sed euismod libero lorem nec arcu. Fusce viverra ipsum in molestie condimentum. Maecenas bibendum diam ligula, ac dapibus ex molestie et. Nam sed velit eu tellus interdum ullamcorper at nec orci. Quisque sagittis enim et ante maximus, non dictum massa elementum.
+
+    Cras ac libero imperdiet, bibendum turpis quis, luctus nibh. Quisque lacinia odio mauris, id tincidunt turpis euismod sed. Donec ultrices rhoncus urna, ac blandit enim aliquam nec. Integer aliquam vel mauris eu ultricies. Nullam orci sem, porttitor id accumsan in, aliquam ut nulla. In iaculis mi sed eros semper rutrum quis sed ex. Suspendisse vulputate erat nec libero tempor consectetur non ac odio. In quis mattis risus, vitae dapibus lorem. Suspendisse elementum sit amet enim id suscipit. Quisque convallis, eros quis laoreet laoreet, risus metus pharetra sem, dictum elementum risus massa at lorem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam laoreet quam in purus facilisis, ac euismod turpis maximus. Fusce auctor et ipsum a lacinia. Vestibulum lobortis dolor id lorem eleifend, sed vulputate neque imperdiet. Curabitur quis aliquet mi.</p>
+  <p>Ut elementum erat sapien, ut accumsan massa aliquam in. Nullam dapibus tortor nec sodales pretium. Duis mattis augue id justo venenatis consectetur. Quisque pulvinar blandit euismod. Fusce et metus nulla. Quisque elementum dolor non diam rutrum, a tincidunt justo sagittis. Nullam ac augue sed massa faucibus pellentesque et sit amet arcu. Quisque iaculis vestibulum justo, sit amet commodo purus accumsan quis. Proin facilisis nulla ut ipsum mollis, vitae auctor nibh laoreet. Praesent ultricies sapien id pellentesque pretium. Suspendisse molestie a purus ac eleifend. Quisque et justo risus. Integer aliquet erat arcu, a tincidunt purus finibus a.
+
+    In dolor sem, bibendum non efficitur ornare, accumsan at urna. Maecenas id eleifend sapien, sed dapibus leo. Donec a arcu nec nunc dignissim bibendum quis in tortor. Nunc et mi et nisl fermentum vulputate nec sed nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus ut hendrerit quam, non ultrices tortor. Nam dolor nisi, vehicula quis lacus et, egestas semper purus. In ultrices viverra feugiat. Sed a sem pretium, imperdiet enim vel, mattis dui. Vestibulum consequat ipsum non magna ullamcorper euismod. Nulla consequat porttitor ligula, sit amet dictum nisl eleifend a.</p>
+</body>
+
+</html>
